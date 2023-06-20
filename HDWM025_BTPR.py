ca39dfe8a94e04f5c5332b05d7f84c61beea5332
nkhagan <nkhagan@ualr.edu>
2023-06-20 14:03:47 -0500

    Version 1.03 Cleaned up splits

--- HDWM025_BTPR.py
+++ HDWM025_BTPR.py
@@ -8,7 +8,7 @@ from operator import itemgetter
  #                     DEVELOPER's NOTES
  #     --BLOCK TOKEN PAIR Reducer-- 
  #  Input comes from output of Block Token Pair Mapper. 
- #  Takes the output from the pairs created in rge BTPM step and 
+ #  Takes the output from the pairs created in the BTPM step and 
  #  reduces based on the pair keys (Blocking Key Pairs) and 
  #  collects all the refIDs that fall under that pair key.
  ##############################################################
@@ -22,7 +22,10 @@ block_keyPairs = None
 for items in sys.stdin:
     line = items.strip()
     #print(line)
-    block_keyPairs, refID = line.split(":")
+    line = line.split(":")
+    block_keyPairs = line[0].strip()
+    refID = line[1].strip()
+    #block_keyPairs, refID = line.split(":")
     #print(current_refID)
     #print(block_keyPairs)
 #-----------------------------------------------------------
@@ -43,11 +46,12 @@ for items in sys.stdin:
                         pair = (Xtoken + ',' + Ytoken)
                         print('%s : %s' % (pair, 'one'))
                     else:
-                        pair2 = (Ytoken + ',' + Xtoken )
-                        print('%s : %s' % (pair2, 'one'))
+                        pair = (Ytoken + ',' + Xtoken )
+                        print('%s : %s' % (pair, 'one'))
         current_refID = refID
         current_block_keyPairs = block_keyPairs
 
+'''
 # Output the last word
 if current_block_keyPairs == block_keyPairs:
 ##    print ('%s : %s' % (current_block_keyPairs, current_refID))
@@ -63,6 +67,7 @@ if current_block_keyPairs == block_keyPairs:
             else:
                 pair2 = (Ytoken + ',' + Xtoken )
                 print('%s : %s' % (pair2, 'one'))
+'''
 ############################################################
 #               END OF REDUCER       
 ############################################################
